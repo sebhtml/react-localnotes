f23f3de539979d89f4adf887b8b13178c2220c3a
AranDR <arangradient@gmail.com>
2018-06-03 02:27:46 +0200

    Include CSS/HTML in the react app

--- app/src/App.js
+++ app/src/App.js
@@ -5,15 +5,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div>
+		<div>
+			<div class="new-button">
+				<span class="plus" id="createNew">+</span>
+				<span class="text">New</span>
+			</div>
+			<div id="past">
+				<h1>Recently written</h1> 
+				<div id="history-previous"></div>
+				<div id="history-next"></div>
+			</div>
+			<div id="container">
+				<h1 id="today"></h1>
+				<textarea id="maintext" class="writehere placeholder" autofocus="true">
+					Start writing...
+				</textarea>
+				<div class="notifications">
+					<p id="message">&nbsp;</p>
+					<p id="autosave">&nbsp;</p>
+				</div>
+			</div>
+		</div>
+		<button id="donewriting" class="action">Done for now</button>
+		<div id="taglist"></div>
+        </div>
     );
   }
 }
